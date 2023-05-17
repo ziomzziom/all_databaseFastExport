e58fd52d45de01c8dc3fccb56be002bcee1a510c
ziomzziom <54291606+ziomzziom@users.noreply.github.com>
2023-05-17 13:04:06 +0200

    default usrnm, passwd values

--- mysqldump.sh
+++ mysqldump.sh
@@ -2,8 +2,8 @@
 
 # Dane logowania do bazy danych
 hostname="localhost"
-username="nazwa_uzytkownika"
-password="haslo"
+username="root"
+password=""
 
 # Pobierz listę wszystkich baz danych
 dbListOutput=$(mysql -u$username -p$password -h$hostname -e "SHOW DATABASES")
